697695f4779ce52e3f4cf1dac0bd138f0f0eaf92
kumarabhishek404 <abhishek19@navgurukul.org>
2020-11-17 13:21:58 +0530

    react-redux-firebase-app

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,52 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase)
+
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello, Abhi! How are you.", {structuredData: true});
+  functions.logger.info("Hello, Abhi! How are you.", { structuredData: true });
   response.send("Hello, Abhi! How are you.")
 });
+
+exports.creatNotification = (notification => {
+  return admin.firestore().collection('notifications')
+    .add(notification)
+    .then(doc => console.log('Notification added', doc))
+})
+
+exports.projectCreated = functions.firebase
+  .document('projects/{projectId}')
+  .onCreate(doc => {
+
+    const project = doc.data();
+    const notification = {
+      content: 'Added a new project',
+      user: `${project.autherFirstName} ${project.autherLastName}`,
+      time: admin.firestore.FieldValue.serverTimestamp()
+    }
+
+    return this.creatNotification(notification)
+
+  })
+
+exports.joinedUser = functions.auth.user()
+  .onCreate(user => {
+
+    return admin.firestore().collection('users')
+      .doc(user.uid)
+      .get()
+      .then(doc => {
+
+        const newUser = doc.data();
+        const notification = {
+          content: 'Joined the party',
+          user: `${newUser.firstName} ${newUser.lastName}`,
+          time: admin.firestore.FieldValue.serverTimestamp()
+        }
+
+        return this.creatNotification(notification)
+
+      })
+
+  })
